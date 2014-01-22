128e0a21f2c73c59785622ce369c168c018a0f9d
3xian <suffixtree@gmail.com>
2014-01-22 15:59:16 +0800

    修正文件格式

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
 $(function() {
 	$('#submitContest').click(function() {
-      $.post('/sc/contest/add', {
-        winners: $('#winners').val(),
-        losers: $('#losers').val()
-      }, function(msg) {
-		if (msg == 'ok') {
-			window.location.reload();
-		} else {
-			alert(msg);
-		}
-      });
+		$.post('/sc/contest/add', {
+			winners: $('#winners').val(),
+			losers: $('#losers').val()
+		}, function(msg) {
+			if (msg == 'ok') {
+				window.location.reload();
+			} else {
+				alert(msg);
+			}
+		});
 	});
 
 	$('#rollback').click(function() {
